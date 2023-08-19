ccd5d379414399322c067171bec458bc02d63763
Mahfuz Alam <hasibul112002@gmail.com>
2023-08-19 14:21:34 +0600

    asyncronous

--- src/module3/asyncronous.ts
+++ src/module3/asyncronous.ts
@@ -1,3 +1,22 @@
+interface ITodo {
+    userId: number,
+    id: number,
+    title: string,
+    completed: boolean
+}
+
+const getTodo = async ():Promise<ITodo> => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    return await response.json();
+}
+
+const getTodoData = async ():Promise<void> => {
+    const result = await getTodo();
+    console.log(result)
+}
+
+getTodoData();
+
 const makePromise = (): Promise<string> => {
     return new Promise<string>((resolve, reject) => {
         const data: string = 'data is fetched';
@@ -9,9 +28,9 @@ const makePromise = (): Promise<string> => {
     });
 };
 
-const makePromiseBoolean = (): Promise<Boolean> => {
-    return new Promise<Boolean>((resolve, reject) => {
-        const data: Boolean = true;
+const makePromiseBoolean = (): Promise<boolean> => {
+    return new Promise<boolean>((resolve, reject) => {
+        const data: boolean = true;
         if (data) {
             resolve(data);
         } else {
@@ -20,12 +39,32 @@ const makePromiseBoolean = (): Promise<Boolean> => {
     });
 };
 
-const getPromiseData = async (): Promise<string> => {
-    const data = await makePromise();
-    return data; 
+interface DataType {
+    data: string
+}
+
+const makePromiseObject = (): Promise<DataType> => {
+    return new Promise<DataType>((resolve, reject) => {
+        const data: DataType = { data: 'data is fetched' };
+        if (data) {
+            resolve(data);
+        } else {
+            reject("data is not fetched");
+        }
+    });
 };
 
-const getPromiseBoolean = async (): Promise<Boolean> => {
+const getPromiseData = async (): Promise<string> => {
+    const data = await makePromise();
+    return data;
+};
+
+const getPromiseObject = async (): Promise<DataType> => {
+    const data = await makePromiseObject();
+    return data;
+};
+
+const getPromiseBoolean = async (): Promise<boolean> => {
     const data = await makePromiseBoolean();
-    return data; 
+    return data;
 };
